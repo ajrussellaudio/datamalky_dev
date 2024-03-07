91879257a32b39fd2e79e1ac8f48d69f4bc1373a
Alan Russell <ajrussellaudio@gmail.com>
2024-03-07 21:03:22 +0000

    s i m p l i f y

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,61 +3,35 @@ import {
   DefinitionList,
   DefinitionTitle,
 } from "@/components/definition";
-import { HoverWord } from "@/components/hover-word";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <main className="h-full grid place-items-center">
-      <TooltipProvider>
-        <h1 className="text-9xl tracking-tighter">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <HoverWord>data</HoverWord>
-            </TooltipTrigger>
-            <TooltipContent>
-              <DefinitionList>
-                <DefinitionTitle>
-                  <strong>data</strong> <em>(n, pl)</em>
-                </DefinitionTitle>
-                <DefinitionDetails>
-                  <em>(collectively, uncountable)</em> Information, especially
-                  in a scientific or computational context, or with the
-                  implication that it is organized.
-                </DefinitionDetails>
-                <DefinitionDetails>
-                  <em>(computing)</em> A representation of facts or ideas in a
-                  formalized manner capable of being communicated or manipulated
-                  by some process.
-                </DefinitionDetails>
-              </DefinitionList>
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <HoverWord>malky</HoverWord>
-            </TooltipTrigger>
-            <TooltipContent>
-              <DefinitionList>
-                <DefinitionTitle>
-                  <strong>malky</strong> <em>(v)</em>
-                </DefinitionTitle>
-                <DefinitionDetails>
-                  <em>(Scottish, slang)</em> To physically assault
-                </DefinitionDetails>
-                <DefinitionDetails>
-                  <em>(Scottish, slang)</em> To murder
-                </DefinitionDetails>
-              </DefinitionList>
-            </TooltipContent>
-          </Tooltip>
-        </h1>
-      </TooltipProvider>
+      <h1 className="sr-only">Datamalky</h1>
+      <DefinitionList>
+        <DefinitionTitle>
+          <strong>data</strong> <em>(n, pl)</em>
+        </DefinitionTitle>
+        <DefinitionDetails>
+          <em>(collectively, uncountable)</em> Information, especially in a
+          scientific or computational context, or with the implication that it
+          is organized.
+        </DefinitionDetails>
+        <DefinitionDetails>
+          <em>(computing)</em> A representation of facts or ideas in a
+          formalized manner capable of being communicated or manipulated by some
+          process.
+        </DefinitionDetails>
+        <DefinitionTitle>
+          <strong>malky</strong> <em>(v)</em>
+        </DefinitionTitle>
+        <DefinitionDetails>
+          <em>(Scottish, slang)</em> To physically assault
+        </DefinitionDetails>
+        <DefinitionDetails>
+          <em>(Scottish, slang)</em> To murder
+        </DefinitionDetails>
+      </DefinitionList>
     </main>
   );
 }
