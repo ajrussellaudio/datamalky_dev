168904e80ee81cf2fb0fb4050fc27881bcdd8f10
Alan Russell <ajrussellaudio@gmail.com>
2024-03-07 21:15:06 +0000

    Hardcode to favourite colours

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
-import {
-  Libre_Baskerville as FontSerif,
-  Inter as FontSans,
-} from "next/font/google";
+import { Libre_Baskerville as FontSerif } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSerif = FontSerif({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-serif",
-});
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -30,15 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("h-full p-4", fontSerif.variable, fontSans.variable)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+      <body
+        className={cn("h-full p-4 bg-[#333333] text-white", fontSerif.variable)}
+      >
+        {children}
       </body>
     </html>
   );
